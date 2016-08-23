434c7115e131b60d7282fd01782302a5fb17ddc4
ptlergo <ptlergo@gmail.com>
2016-08-22 21:23:52 -0400

    warn function cleanup

--- src/index.js
+++ src/index.js
@@ -102,54 +102,37 @@ exports.error = (msg, obj, level) => {
   }
 };
 
-  /**
-   * warn() will output to console.warn()
-   *
-   * @param {String} msg
-   * @param {Object} obj
-   * @param {Variable} level
-   * @return {String} message
-   */
-   // Create new module
-   exports.warn = (msg, obj, level) => {
-     // Get debug status
-     const debugging = process.env.DEBUG;
+/**
+  * warn() will output to console.warn()
+  *
+  * @param {String} msg
+ * @param {Object} obj
+ * @param {Variable} level
+ * @return {String} message
+ */
+exports.warn = (msg, obj, level) => {
+  // Get debug status
+  const debugging = process.env.DEBUG;
 
-     if (debugging) {
-       // Get current time
-       const timestamp = new Date();
-       let text;
+  if (debugging) {
+      // Get current time
+    const timestamp = new Date();
+    let text;
 
-       // Make a string of our text
-       text = `\n${timestamp}\n${msg}\n`;
-       if (obj) {
-         text += JSON.stringify(obj, null, 4);
-       }
+    // Make a string of our text
+    text = `\n${timestamp}\n${msg}\n`;
+    if (obj) {
+      text += JSON.stringify(obj, null, 4);
+    }
 
-       // Color text depending on level
-       if (level === 0) {
-         text = error(text);
-       } else if (level === 1) {
-         text = success(text);
-       }
+      // Color text depending on level
+    if (level === 0) {
+      text = error(text);
+    } else if (level === 1) {
+      text = success(text);
+    }
 
-       // Print to console
-       console.log(`${text}\n`);
-
-       // Print (append) to file
-       text = `<li style="background:purple;color:skyblue;font-style:italic">
-       <p>${timestamp}</p>
-         <p>${msg}</p>
-         <p>${JSON.stringify(obj, null, 4)}</p>
-   </li>
-   <hr style=background:yellow>`;
-
-       // Print text out to the debug file
-       fs.appendFile('logs/debug.log', text, (err) => {
-         // Display error in console if it is not null
-         if (err) {
-           console.log(err);
-         }
-       });
-     }
-   };
+    // Print to console
+    console.log(`${text}\n`);
+  }
+};
