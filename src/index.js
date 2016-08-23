7a1566af1cedaba66654f2ab4866071bde4d4134
ptlergo <ptlergo@gmail.com>
2016-08-22 21:17:05 -0400

    clean up log function

--- src/index.js
+++ src/index.js
@@ -62,48 +62,32 @@ exports.debug = (msg, obj, level) => {
  * @return {String} message
  */
  // Create new module
- exports.log = (msg, obj, level) => {
-   // Get debug status
-   const debugging = process.env.DEBUG;
+exports.log = (msg, obj, level) => {
+  // Get debug status
+  const debugging = process.env.DEBUG;
 
-   if (debugging) {
-     // Get current time
-     const timestamp = new Date();
-     let text;
+  if (debugging) {
+  // Get current time
+    const timestamp = new Date();
+    let text;
 
-     // Make a string of our text
-     text = `\n${timestamp}\n${msg}\n`;
-     if (obj) {
-       text += JSON.stringify(obj, null, 4);
-     }
+    // Make a string of our text
+    text = `\n${timestamp}\n${msg}\n`;
+    if (obj) {
+      text += JSON.stringify(obj, null, 4);
+    }
 
      // Color text depending on level
-     if (level === 0) {
-       text = error(text);
-     } else if (level === 1) {
-       text = success(text);
-     }
+    if (level === 0) {
+      text = error(text);
+    } else if (level === 1) {
+      text = success(text);
+    }
 
-     // Print to console
-     console.log(`${text}\n`);
-
-     // Print (append) to file
-     text = `<li style="background:purple;color:skyblue;font-style:italic">
-     <p>${timestamp}</p>
-       <p>${msg}</p>
-       <p>${JSON.stringify(obj, null, 4)}</p>
- </li>
- <hr style=background:yellow>`;
-
-     // Print text out to the debug file
-     fs.appendFile('logs/debug.log', text, (err) => {
-       // Display error in console if it is not null
-       if (err) {
-         console.log(err);
-       }
-     });
-   }
- };
+    // Print to console
+    console.log(`${text}\n`);
+  }
+};
 
  /**
   * error() will output to console.error()
