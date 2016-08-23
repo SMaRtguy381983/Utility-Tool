b4f36ee2da9ec1ff426f253428377a13b3c925e0
ptlergo <ptlergo@gmail.com>
2016-08-22 21:14:00 -0400

    dublicated debug function for other functions

--- src/index.js
+++ src/index.js
@@ -61,8 +61,49 @@ exports.debug = (msg, obj, level) => {
  * @param {Variable} level
  * @return {String} message
  */
-exports.log = (msg, obj, level) => {
-};
+ // Create new module
+ exports.log = (msg, obj, level) => {
+   // Get debug status
+   const debugging = process.env.DEBUG;
+
+   if (debugging) {
+     // Get current time
+     const timestamp = new Date();
+     let text;
+
+     // Make a string of our text
+     text = `\n${timestamp}\n${msg}\n`;
+     if (obj) {
+       text += JSON.stringify(obj, null, 4);
+     }
+
+     // Color text depending on level
+     if (level === 0) {
+       text = error(text);
+     } else if (level === 1) {
+       text = success(text);
+     }
+
+     // Print to console
+     console.log(`${text}\n`);
+
+     // Print (append) to file
+     text = `<li style="background:purple;color:skyblue;font-style:italic">
+     <p>${timestamp}</p>
+       <p>${msg}</p>
+       <p>${JSON.stringify(obj, null, 4)}</p>
+ </li>
+ <hr style=background:yellow>`;
+
+     // Print text out to the debug file
+     fs.appendFile('logs/debug.log', text, (err) => {
+       // Display error in console if it is not null
+       if (err) {
+         console.log(err);
+       }
+     });
+   }
+ };
 
  /**
   * error() will output to console.error()
@@ -72,9 +113,49 @@ exports.log = (msg, obj, level) => {
   * @param {Variable} level
   * @return {String} message
   */
+  // Create new module
   exports.error = (msg, obj, level) => {
-  };
+    // Get debug status
+    const debugging = process.env.DEBUG;
 
+    if (debugging) {
+      // Get current time
+      const timestamp = new Date();
+      let text;
+
+      // Make a string of our text
+      text = `\n${timestamp}\n${msg}\n`;
+      if (obj) {
+        text += JSON.stringify(obj, null, 4);
+      }
+
+      // Color text depending on level
+      if (level === 0) {
+        text = error(text);
+      } else if (level === 1) {
+        text = success(text);
+      }
+
+      // Print to console
+      console.log(`${text}\n`);
+
+      // Print (append) to file
+      text = `<li style="background:purple;color:skyblue;font-style:italic">
+      <p>${timestamp}</p>
+        <p>${msg}</p>
+        <p>${JSON.stringify(obj, null, 4)}</p>
+  </li>
+  <hr style=background:yellow>`;
+
+      // Print text out to the debug file
+      fs.appendFile('logs/debug.log', text, (err) => {
+        // Display error in console if it is not null
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  };
 
   /**
    * warn() will output to console.warn()
@@ -84,5 +165,46 @@ exports.log = (msg, obj, level) => {
    * @param {Variable} level
    * @return {String} message
    */
+   // Create new module
    exports.warn = (msg, obj, level) => {
+     // Get debug status
+     const debugging = process.env.DEBUG;
+
+     if (debugging) {
+       // Get current time
+       const timestamp = new Date();
+       let text;
+
+       // Make a string of our text
+       text = `\n${timestamp}\n${msg}\n`;
+       if (obj) {
+         text += JSON.stringify(obj, null, 4);
+       }
+
+       // Color text depending on level
+       if (level === 0) {
+         text = error(text);
+       } else if (level === 1) {
+         text = success(text);
+       }
+
+       // Print to console
+       console.log(`${text}\n`);
+
+       // Print (append) to file
+       text = `<li style="background:purple;color:skyblue;font-style:italic">
+       <p>${timestamp}</p>
+         <p>${msg}</p>
+         <p>${JSON.stringify(obj, null, 4)}</p>
+   </li>
+   <hr style=background:yellow>`;
+
+       // Print text out to the debug file
+       fs.appendFile('logs/debug.log', text, (err) => {
+         // Display error in console if it is not null
+         if (err) {
+           console.log(err);
+         }
+       });
+     }
    };
