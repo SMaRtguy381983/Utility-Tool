9df691082fb9ed3e64ea9ebe9b5c5abe5bfee670
ptlergo <ptlergo@gmail.com>
2016-08-24 01:03:16 -0400

    stubs updated with constants

--- test/__test.js
+++ test/__test.js
@@ -56,19 +56,28 @@ describe('Utility Tool', () => {
 // Unit test for version bumping
 describe('version bump test', () => {
   it('should test the MAJOR bump', (done) => {
-    const bumpit = bump.bumpit('v', 'MAJOR');
+    const currentV = '0.0.0';
+    const tag = 'major';
+    const bumpit = bump.bumpit(currentV, tag);
+
     console.log(bumpit);
     done();
   });
 
   it('should test the MINOR bump', (done) => {
-    const bumpit = bump.bumpit('v', 'MINOR');
+    const currentV = '0.0.0';
+    const tag = 'major';
+    const bumpit = bump.bumpit(currentV, tag);
+
     console.log(bumpit);
     done();
   });
 
   it('should test the PATCH bump', (done) => {
-    const bumpit = bump.bumpit('v', 'PATCH');
+    const currentV = '0.0.0';
+    const tag = 'major';
+    const bumpit = bump.bumpit(currentV, tag);
+
     console.log(bumpit);
     done();
   });
